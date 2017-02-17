17a69fe8ce4c6958a78d06c00c6dfaed3842252c
Bruno Gola <me@bgo.la>
2017-02-17 11:02:06 +0100

    novo link para download

--- js/represent.js
+++ js/represent.js
@@ -56,7 +56,7 @@ function init(){
   intro.onclick = new_game;
   var fb = document.getElementById("facebook").onclick = function(){ window.open("http://www.facebook.com/sharer.php?u=http://www.justintype.com/represent")};
   var tw = document.getElementById("twitter").onclick = function () { window.open("http://www.facebook.com/sharer.php?u=http://www.justintype.com/represent")};
-  download.onclick = function () { window.open("http://www.justintype.com/free/represent/RepresentSans-Regular.otf.zip")};
+  download.onclick = function () { window.open("http://www.justintype.com.br/free/represent/RepresentSans-Regular.otf.zip")};
   tryagain.onclick = function () {
     gameover.setAttribute("style", "display:none");
     intro.setAttribute("style", "display:block");
